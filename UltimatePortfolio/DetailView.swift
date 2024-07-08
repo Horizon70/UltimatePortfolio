6c546292a71b2060fbfcb5e89570761e015d83d6
Davide Fabretti <davide@Mac-mini-di-Davide.local>
2024-07-08 15:49:11 +0200

    Initial commit.

---
+++ UltimatePortfolio/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  UltimatePortfolio
+//
+//  Created by Davide Fabretti on 07/07/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,27 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Davide Fabretti on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    @StateObject var dataController = DataController()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationSplitView {
+                SidebarView()
+            } content: {
+                ContentView()
+            } detail: {
+                DetailView()
+            }
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+        }
+    }
+}
